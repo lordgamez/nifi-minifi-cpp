bd55bfa90f7b5be217e605ef6ea2c873e90c6c09
Gabor Gyimesi <gamezbird@gmail.com>
2021-06-30 10:39:32 +0200

    Use exec_run from docker API instead of subprocess

--- docker/test/integration/minifi/core/DockerTestCluster.py
+++ docker/test/integration/minifi/core/DockerTestCluster.py
@@ -77,37 +77,53 @@ class DockerTestCluster(SingleNodeDockerCluster):
                 for line in logs.decode("utf-8").splitlines():
                     logging.info(line)
 
-    def check_http_proxy_access(self, url):
-        output = subprocess.check_output(["docker", "exec", "http-proxy", "cat", "/var/log/squid/access.log"]).decode(self.get_stdout_encoding())
-        return url in output \
+    def check_http_proxy_access(self, container_name, url):
+        (code, output) = self.client.containers.get(container_name).exec_run(["cat", "/var/log/squid/access.log"])
+        output = output.decode(self.get_stdout_encoding())
+        return code == 0 and url in output \
             and ((output.count("TCP_DENIED/407") != 0
-                  and output.count("TCP_MISS") == output.count("TCP_DENIED/407"))
+                 and output.count("TCP_MISS") == output.count("TCP_DENIED/407"))
                  or output.count("TCP_DENIED/407") == 0 and "TCP_MISS" in output)
 
     @retry_check()
-    def check_s3_server_object_data(self, test_data):
-        s3_mock_dir = subprocess.check_output(["docker", "exec", "s3-server", "find", "/tmp/", "-type", "d", "-name", "s3mock*"]).decode(self.get_stdout_encoding()).strip()
-        file_data = subprocess.check_output(["docker", "exec", "s3-server", "cat", s3_mock_dir + "/test_bucket/test_object_key/fileData"]).decode(self.get_stdout_encoding())
-        return file_data == test_data
+    def check_s3_server_object_data(self, container_name, test_data):
+        (code, output) = self.client.containers.get(container_name).exec_run(["find", "/tmp/", "-type", "d", "-name", "s3mock*"])
+        if code != 0:
+            return False
+        s3_mock_dir = output.decode(self.get_stdout_encoding()).strip()
+        (code, output) = self.client.containers.get(container_name).exec_run(["cat", s3_mock_dir + "/test_bucket/test_object_key/fileData"])
+        file_data = output.decode(self.get_stdout_encoding())
+        return code == 0 and file_data == test_data
 
     @retry_check()
-    def check_s3_server_object_metadata(self, content_type="application/octet-stream", metadata=dict()):
-        s3_mock_dir = subprocess.check_output(["docker", "exec", "s3-server", "find", "/tmp/", "-type", "d", "-name", "s3mock*"]).decode(self.get_stdout_encoding()).strip()
-        metadata_json = subprocess.check_output(["docker", "exec", "s3-server", "cat", s3_mock_dir + "/test_bucket/test_object_key/metadata"]).decode(self.get_stdout_encoding())
-        server_metadata = json.loads(metadata_json)
-        return server_metadata["contentType"] == content_type and metadata == server_metadata["userMetadata"]
+    def check_s3_server_object_metadata(self, container_name, content_type="application/octet-stream", metadata=dict()):
+        (code, output) = self.client.containers.get(container_name).exec_run(["find", "/tmp/", "-type", "d", "-name", "s3mock*"])
+        if code != 0:
+            return False
+        s3_mock_dir = output.decode(self.get_stdout_encoding()).strip()
+        (code, output) = self.client.containers.get(container_name).exec_run(["cat", s3_mock_dir + "/test_bucket/test_object_key/metadata"])
+        server_metadata = json.loads(output.decode(self.get_stdout_encoding()))
+        return code == 0 and server_metadata["contentType"] == content_type and metadata == server_metadata["userMetadata"]
 
     @retry_check()
-    def check_azure_storage_server_data(self, test_data):
-        data_file = subprocess.check_output(["docker", "exec", "azure-storage-server", "find", "/data/__blobstorage__", "-type", "f"]).decode(self.get_stdout_encoding()).strip()
-        file_data = subprocess.check_output(["docker", "exec", "azure-storage-server", "cat", data_file]).decode(self.get_stdout_encoding())
-        return test_data in file_data
+    def check_azure_storage_server_data(self, container_name, test_data):
+        (code, output) = self.client.containers.get(container_name).exec_run(["find", "/data/__blobstorage__", "-type", "f"])
+        if code != 0:
+            return False
+        data_file = output.decode(self.get_stdout_encoding()).strip()
+        (code, output) = self.client.containers.get(container_name).exec_run(["cat", data_file])
+        file_data = output.decode(self.get_stdout_encoding())
+        return code == 0 and test_data in file_data
 
     @retry_check()
-    def is_s3_bucket_empty(self):
-        s3_mock_dir = subprocess.check_output(["docker", "exec", "s3-server", "find", "/tmp/", "-type", "d", "-name", "s3mock*"]).decode(self.get_stdout_encoding()).strip()
-        ls_result = subprocess.check_output(["docker", "exec", "s3-server", "ls", s3_mock_dir + "/test_bucket/"]).decode(self.get_stdout_encoding())
-        return not ls_result
+    def is_s3_bucket_empty(self, container_name):
+        (code, output) = self.client.containers.get(container_name).exec_run(["find", "/tmp/", "-type", "d", "-name", "s3mock*"])
+        if code != 0:
+            return False
+        s3_mock_dir = output.decode(self.get_stdout_encoding()).strip()
+        (code, output) = self.client.containers.get(container_name).exec_run(["ls", s3_mock_dir + "/test_bucket/"])
+        ls_result = output.decode(self.get_stdout_encoding())
+        return code == 0 and not ls_result
 
     def segfault_happened(self):
         return self.segfault
