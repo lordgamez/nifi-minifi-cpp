f00c7dd6de847bb1e5f7b88c0f9c86ce0d0b03d7
Gabor Gyimesi <gamezbird@gmail.com>
2021-07-15 11:02:02 +0200

    Minor refactor

--- libminifi/include/core/yaml/YamlConnectionParser.h
+++ libminifi/include/core/yaml/YamlConnectionParser.h
@@ -54,7 +54,7 @@ class YamlConnectionParser {
 
  private:
   void addNewRelationshipToConnection(const std::string& relationship_name, const std::shared_ptr<minifi::Connection>& connection) const;
-  void addFunnelConnection(const std::shared_ptr<minifi::Connection>& connection) const;
+  void addFunnelRelationshipToConnection(const std::shared_ptr<minifi::Connection>& connection) const;
 
   const YAML::Node& connectionNode_;
   const std::string& name_;
