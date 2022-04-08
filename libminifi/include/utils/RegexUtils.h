69625bcd775a500961a3428e99e450aa19691705
Gabor Gyimesi <gamezbird@gmail.com>
2022-04-08 14:30:51 +0200

    Add regexutils

---
+++ libminifi/include/utils/RegexUtils.h
@@ -0,0 +1,127 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <string>
+#include <string_view>
+#include <vector>
+
+#if defined(__GNUC__)
+#include <regex.h>
+#else
+#include <regex>
+#define NO_MORE_REGFREEE
+#endif
+
+namespace org::apache::nifi::minifi::utils {
+
+class Regex;
+
+class SMatch {
+ private:
+#ifdef NO_MORE_REGFREEE
+  std::smatch matches_;
+#else
+  struct RegmatchWrapper {
+    operator std::string() const {
+      return str();
+    }
+
+    std::string str() const {
+      if (match.rm_so == -1) {
+        return "";
+      }
+      return std::string(pattern.begin() + match.rm_so, pattern.begin() + match.rm_eo);
+    }
+
+    const regmatch_t& match;
+    std::string_view pattern;
+  };
+
+  struct SuffixWrapper {
+    operator std::string() const {
+      return str();
+    }
+
+    std::string str() const {
+      return suffix;
+    }
+
+    std::string suffix;
+  };
+  std::vector<regmatch_t> matches_;
+  std::string pattern_;
+#endif
+
+  friend bool regexMatch(const std::string &pattern, SMatch& match, const Regex& regex);
+  friend bool regexSearch(const std::string &pattern, SMatch& match, const Regex& regex);
+
+public:
+#ifdef NO_MORE_REGFREEE
+  const decltype(matches_.suffix())& suffix() const;
+  const decltype(matches_[0])& operator[](std::size_t index) const;
+#else
+  SuffixWrapper suffix() const;
+  RegmatchWrapper operator[](std::size_t index) const;
+#endif
+  std::size_t size() const;
+};
+
+class Regex {
+ public:
+  enum class Mode { ICASE };
+
+  Regex();
+  explicit Regex(const std::string &value);
+  explicit Regex(const std::string &value,
+                const std::vector<Mode> &mode);
+  Regex(const Regex &);
+  Regex& operator=(const Regex &);
+  Regex(Regex&& other);
+  Regex& operator=(Regex&& other);
+  ~Regex();
+
+ private:
+  std::string regex_str_;
+  bool valid_;
+
+#ifdef NO_MORE_REGFREEE
+  std::regex compiled_regex_;
+  std::regex_constants::syntax_option_type regex_mode_;
+#else
+
+  regex_t compiled_regex_;
+  regex_t compiled_full_input_regex_;
+  int regex_mode_;
+#endif
+
+  friend class SMatch;
+  friend bool regexMatch(const std::string &pattern, const Regex& regex);
+  friend bool regexMatch(const std::string &pattern, SMatch& match, const Regex& regex);
+  friend bool regexSearch(const std::string &pattern, const Regex& regex);
+  friend bool regexSearch(const std::string &pattern, SMatch& match, const Regex& regex);
+};
+
+bool regexMatch(const std::string &pattern, const Regex& regex);
+bool regexMatch(const std::string &pattern, SMatch& match, const Regex& regex);
+
+bool regexSearch(const std::string &pattern, const Regex& regex);
+bool regexSearch(const std::string &pattern, SMatch& match, const Regex& regex);
+
+}  // namespace org::apache::nifi::minifi::utils
---
+++ libminifi/src/utils/RegexUtils.cpp
@@ -0,0 +1,255 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "utils/RegexUtils.h"
+#include "Exception.h"
+#include <iostream>
+#include <vector>
+
+namespace org::apache::nifi::minifi::utils {
+
+#ifdef NO_MORE_REGFREEE
+const decltype(SMatch::matches_.suffix())& SMatch::suffix() const {
+  return matches_.suffix();
+#else
+SMatch::SuffixWrapper SMatch::suffix() const {
+  if ((size_t) matches_[0].rm_eo >= pattern_.size()) {
+    return SuffixWrapper{std::string()};
+  } else {
+    return SuffixWrapper{pattern_.substr(matches_[0].rm_eo)};
+  }
+#endif
+}
+
+#ifdef NO_MORE_REGFREEE
+const decltype(SMatch::matches_[0])& SMatch::operator[](std::size_t index) const {
+  return matches_[index];
+#else
+SMatch::RegmatchWrapper SMatch::operator[](std::size_t index) const {
+  return RegmatchWrapper{matches_[index], pattern_};
+#endif
+}
+
+std::size_t SMatch::size() const {
+#ifdef NO_MORE_REGFREEE
+  return matches_.size();
+#else
+  std::size_t count = 0;
+  for (const auto &m : matches_) {
+    if (m.rm_so == -1) {
+      break;
+    }
+    ++count;
+  }
+  return count;
+#endif
+}
+
+Regex::Regex() : Regex::Regex("") {}
+
+Regex::Regex(const std::string &value) : Regex::Regex(value, {}) {}
+
+Regex::Regex(const std::string &value,
+             const std::vector<Regex::Mode> &mode)
+    : regex_str_(value),
+      valid_(false) {
+  // Create regex mode
+#ifdef NO_MORE_REGFREEE
+  regex_mode_ = std::regex_constants::ECMAScript;
+#else
+  regex_mode_ = REG_EXTENDED;
+#endif
+  for (const auto m : mode) {
+    switch (m) {
+      case Mode::ICASE:
+#ifdef NO_MORE_REGFREEE
+        regex_mode_ |= std::regex_constants::icase;
+#else
+        regex_mode_ |= REG_ICASE;
+#endif
+        break;
+    }
+  }
+  // Compile
+#ifdef NO_MORE_REGFREEE
+  try {
+    compiled_regex_ = std::regex(regex_str_, regex_mode_);
+    valid_ = true;
+  } catch (const std::regex_error &e) {
+    throw Exception(REGEX_EXCEPTION, e.what());
+  }
+#else
+  int err_code = regcomp(&compiled_regex_, regex_str_.c_str(), regex_mode_);
+  if (err_code) {
+    const size_t sz = regerror(err_code, &compiled_regex_, nullptr, 0);
+    std::vector<char> msg(sz);
+    regerror(err_code, &compiled_regex_, msg.data(), msg.size());
+    throw Exception(REGEX_EXCEPTION, std::string(msg.begin(), msg.end()));
+  }
+  err_code = regcomp(&compiled_full_input_regex_, ('^' + regex_str_ + '$').c_str(), regex_mode_);
+  if (err_code) {
+    const size_t sz = regerror(err_code, &compiled_full_input_regex_, nullptr, 0);
+    std::vector<char> msg(sz);
+    regerror(err_code, &compiled_full_input_regex_, msg.data(), msg.size());
+    throw Exception(REGEX_EXCEPTION, std::string(msg.begin(), msg.end()));
+  }
+  valid_ = true;
+#endif
+}
+
+Regex::Regex(const Regex& other)
+#ifndef NO_MORE_REGFREEE
+  : valid_(false),
+    regex_mode_(REG_EXTENDED)
+#endif
+{
+  *this = other;
+}
+
+Regex& Regex::operator=(const Regex& other) {
+  if (this == &other) {
+    return *this;
+  }
+
+  regex_str_ = other.regex_str_;
+#ifdef NO_MORE_REGFREEE
+  compiled_regex_ = other.compiled_regex_;
+  regex_mode_ = other.regex_mode_;
+#else
+  if (valid_) {
+    regfree(&compiled_regex_);
+    regfree(&compiled_full_input_regex_);
+  }
+  regex_mode_ = other.regex_mode_;
+  int err_code = regcomp(&compiled_regex_, regex_str_.c_str(), regex_mode_);
+  if (err_code) {
+    const size_t sz = regerror(err_code, &compiled_regex_, nullptr, 0);
+    std::vector<char> msg(sz);
+    regerror(err_code, &compiled_regex_, msg.data(), msg.size());
+    throw Exception(REGEX_EXCEPTION, std::string(msg.begin(), msg.end()));
+  }
+  err_code = regcomp(&compiled_full_input_regex_, ('^' + regex_str_ + '$').c_str(), regex_mode_);
+  if (err_code) {
+    const size_t sz = regerror(err_code, &compiled_full_input_regex_, nullptr, 0);
+    std::vector<char> msg(sz);
+    regerror(err_code, &compiled_full_input_regex_, msg.data(), msg.size());
+    throw Exception(REGEX_EXCEPTION, std::string(msg.begin(), msg.end()));
+  }
+#endif
+  valid_ = other.valid_;
+  return *this;
+}
+
+Regex::Regex(Regex&& other)
+#ifndef NO_MORE_REGFREEE
+  : valid_(false),
+    regex_mode_(REG_EXTENDED)
+#endif
+{
+  *this = std::move(other);
+}
+
+Regex& Regex::operator=(Regex&& other) {
+  if (this == &other) {
+    return *this;
+  }
+
+  regex_str_ = std::move(other.regex_str_);
+#ifdef NO_MORE_REGFREEE
+  compiled_regex_ = std::move(other.compiled_regex_);
+  regex_mode_ = other.regex_mode_;
+#else
+  if (valid_) {
+    regfree(&compiled_regex_);
+    regfree(&compiled_full_input_regex_);
+  }
+  compiled_regex_ = other.compiled_regex_;
+  compiled_full_input_regex_ = other.compiled_full_input_regex_;
+  regex_mode_ = other.regex_mode_;
+#endif
+  valid_ = other.valid_;
+  other.valid_ = false;
+  return *this;
+}
+
+Regex::~Regex() {
+#ifndef NO_MORE_REGFREEE
+  if (valid_) {
+    regfree(&compiled_regex_);
+    regfree(&compiled_full_input_regex_);
+  }
+#endif
+}
+
+bool regexSearch(const std::string &pattern, const Regex& regex) {
+  if (!regex.valid_) {
+    return false;
+  }
+#ifdef NO_MORE_REGFREEE
+  return std::regex_search(pattern, regex.compiled_regex_);
+#else
+  std::vector<regmatch_t> match;
+  int maxGroups = std::count(regex.regex_str_.begin(), regex.regex_str_.end(), '(') + 1;
+  match.resize(maxGroups);
+  return regexec(&regex.compiled_regex_, pattern.c_str(), match.size(), match.data(), 0) == 0;
+#endif
+}
+
+bool regexSearch(const std::string &pattern, SMatch& match, const Regex& regex) {
+  if (!regex.valid_) {
+    return false;
+  }
+#ifdef NO_MORE_REGFREEE
+  return std::regex_search(pattern,  match.matches_, regex.compiled_regex_);
+#else
+  int maxGroups = std::count(regex.regex_str_.begin(), regex.regex_str_.end(), '(') + 1;
+  match.matches_.resize(maxGroups);
+  match.pattern_ = pattern;
+  return regexec(&regex.compiled_regex_, pattern.c_str(), match.matches_.size(), match.matches_.data(), 0) == 0;
+#endif
+}
+
+bool regexMatch(const std::string &pattern, const Regex& regex) {
+  if (!regex.valid_) {
+    return false;
+  }
+#ifdef NO_MORE_REGFREEE
+  return std::regex_match(pattern, regex.compiled_regex_);
+#else
+  std::vector<regmatch_t> match;
+  int maxGroups = std::count(regex.regex_str_.begin(), regex.regex_str_.end(), '(') + 1;
+  match.resize(maxGroups);
+  return regexec(&regex.compiled_full_input_regex_, pattern.c_str(), match.size(), match.data(), 0) == 0;
+#endif
+}
+
+bool regexMatch(const std::string &pattern, SMatch& match, const Regex& regex) {
+  if (!regex.valid_) {
+    return false;
+  }
+#ifdef NO_MORE_REGFREEE
+  return std::regex_match(pattern, match.matches_, regex.compiled_regex_);
+#else
+  int maxGroups = std::count(regex.regex_str_.begin(), regex.regex_str_.end(), '(') + 1;
+  match.matches_.resize(maxGroups);
+  match.pattern_ = pattern;
+  return regexec(&regex.compiled_full_input_regex_, pattern.c_str(), match.matches_.size(), match.matches_.data(), 0) == 0;
+#endif
+}
+
+}  // namespace org::apache::nifi::minifi::utils
