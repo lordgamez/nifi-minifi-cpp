f166468ea65d368cf5ec9f730dd065d1885296e5
Gabor Gyimesi <gamezbird@gmail.com>
2023-12-13 17:02:24 +0100

    fix tests

--- extensions/python/types/PyProcessor.cpp
+++ extensions/python/types/PyProcessor.cpp
@@ -118,10 +118,14 @@ PyObject* PyProcessor::addProperty(PyProcessor* self, PyObject* args) {
   }
   bool is_required = getBoolFromTuple(args, 3);
   bool supports_expression_language = getBoolFromTuple(args, 4);
-  auto validator_value_pyint = BorrowedLong::fromTuple(args, 5);
+
   std::optional<int64_t> validator_value;
-  if (validator_value_pyint.get() && validator_value_pyint.get() != Py_None) {
-    validator_value = validator_value_pyint.asInt64();
+  auto arg_size = PyTuple_Size(args);
+  if (arg_size > 5) {
+    auto validator_value_pyint = BorrowedLong::fromTuple(args, 5);
+    if (validator_value_pyint.get() && validator_value_pyint.get() != Py_None) {
+      validator_value = validator_value_pyint.asInt64();
+    }
   }
 
   processor->addProperty(name.toUtf8String(), description.toUtf8String(), default_value, is_required, supports_expression_language, validator_value);
