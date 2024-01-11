acd393e3a15a91fa5c08907681dbda50e8770861
Gabor Gyimesi <gamezbird@gmail.com>
2024-01-11 13:09:16 +0100

    MINIFICPP-2277 Add virtualenv support for python processors

--- libminifi/include/properties/Configuration.h
+++ libminifi/include/properties/Configuration.h
@@ -194,6 +194,10 @@ class Configuration : public Properties {
   static constexpr const char *controller_socket_port = "controller.socket.port";
   static constexpr const char *controller_ssl_context_service = "controller.ssl.context.service";
 
+  static constexpr const char *nifi_python_virtualenv_directory = "nifi.python.virtualenv.directory";
+  static constexpr const char *nifi_python_env_setup_binary = "nifi.python.env.setup.binary";
+  static constexpr const char *nifi_python_install_packages_automatically = "nifi.python.install.packages.automatically";
+
   MINIFIAPI static const std::unordered_map<std::string_view, gsl::not_null<const core::PropertyValidator*>> CONFIGURATION_PROPERTIES;
   MINIFIAPI static const std::array<const char*, 2> DEFAULT_SENSITIVE_PROPERTIES;
 
