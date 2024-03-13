1e0eacf9024b07521d2bcc01fca747b8b339aeba
Gabor Gyimesi <gamezbird@gmail.com>
2024-03-13 09:47:35 +0100

    extract python interpreter

--- extensions/python/PythonScriptEngine.cpp
+++ extensions/python/PythonScriptEngine.cpp
@@ -17,7 +17,6 @@
 #include <string>
 
 #include "PythonScriptEngine.h"
-#include "PythonBindings.h"
 #include "PythonConfigState.h"
 #include "types/PyProcessSession.h"
 #include "types/PyProcessContext.h"
@@ -26,61 +25,6 @@
 #include "types/PyRelationship.h"
 
 namespace org::apache::nifi::minifi::extensions::python {
-
-Interpreter* Interpreter::getInterpreter() {
-  static Interpreter interpreter;
-  return &interpreter;
-}
-
-GlobalInterpreterLock::GlobalInterpreterLock()
-    : gil_state_(PyGILState_Ensure()) {
-}
-
-GlobalInterpreterLock::~GlobalInterpreterLock() {
-  PyGILState_Release(gil_state_);
-}
-
-namespace {
-// PyEval_InitThreads might be marked deprecated (depending on the version of Python.h)
-// Python <= 3.6: This needs to be called manually after Py_Initialize to initialize threads
-// Python >= 3.7: Noop function since its functionality is included in Py_Initialize
-// Python >= 3.9: Marked as deprecated (still noop)
-// This can be removed if we drop the support for Python 3.6
-void initThreads() {
-#if defined(__clang__)
-  #pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-#elif defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#elif defined(WIN32)
-  #pragma warning(push)
-#pragma warning(disable: 4996)
-#endif
-  if (!PyEval_ThreadsInitialized())
-    PyEval_InitThreads();
-#if defined(__clang__)
-#pragma clang diagnostic pop
-#elif defined(__GNUC__)
-#pragma GCC diagnostic pop
-#elif defined(WIN32)
-#pragma warning(pop)
-#endif
-}
-
-}  // namespace
-
-Interpreter::Interpreter() {
-  Py_Initialize();
-  initThreads();
-  PyInit_minifi_native();
-  saved_thread_state_ = PyEval_SaveThread();  // NOLINT(cppcoreguidelines-prefer-member-initializer)
-}
-
-Interpreter::~Interpreter() {
-  PyEval_RestoreThread(saved_thread_state_);
-  Py_Finalize();
-}
 
 PythonScriptEngine::PythonScriptEngine() {
   Interpreter::getInterpreter();
