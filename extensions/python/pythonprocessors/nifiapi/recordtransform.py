0fdd7bbbfe481df6b1552c855a2652cf7b17e176
Gabor Gyimesi <gamezbird@gmail.com>
2024-08-26 19:09:14 +0200

    try

--- extensions/python/pythonprocessors/nifiapi/recordtransform.py
+++ extensions/python/pythonprocessors/nifiapi/recordtransform.py
@@ -17,16 +17,13 @@ import traceback
 import json
 from abc import abstractmethod
 from minifi_native import ProcessContext, ProcessSession, Processor
-from .processorbase import ProcessorBase, WriteCallback
+from .processorbase import ProcessorBase
 from .properties import FlowFile as FlowFileProxy
 from .properties import ProcessContext as ProcessContextProxy
 from .properties import PropertyDescriptor
 
 
 class __RecordTransformResult__:
-    class Java:
-        implements = ['org.apache.nifi.python.processor.RecordTransformResult']
-
     def __init__(self, processor_result, recordJson):
         self.processor_result = processor_result
         self.recordJson = recordJson
@@ -45,7 +42,6 @@ class __RecordTransformResult__:
 
 
 class RecordTransformResult:
-
     def __init__(self, record=None, schema=None, relationship="success", partition=None):
         self.record = record
         self.schema = schema
@@ -63,27 +59,6 @@ class RecordTransformResult:
 
     def getPartition(self):
         return self.partition
-
-
-class CachingAttributeMap:
-    cache = None
-
-    def __init__(self, delegate):
-        self.delegate = delegate
-
-    def getAttribute(self, attributeName):
-        # Lazily initialize cache
-        if self.cache is None:
-            self.cache = {}
-        if attributeName in self.cache:
-            return self.cache[attributeName]
-
-        value = self.delegate.getAttribute(attributeName)
-        self.cache[attributeName] = value
-        return value
-
-    def getAttributes(self):
-        return self.delegate.getAttributes()
 
 
 class RecordTransform(ProcessorBase):
@@ -107,69 +82,59 @@ class RecordTransform(ProcessorBase):
         processor.addProperty(self.RECORD_READER.name, self.RECORD_READER.description, None, self.RECORD_READER.required, False, False, None, self.RECORD_READER.controllerServiceDefinition)
         processor.addProperty(self.RECORD_WRITER.name, self.RECORD_WRITER.description, None, self.RECORD_WRITER.required, False, False, None, self.RECORD_WRITER.controllerServiceDefinition)
 
-    def transformRecord(self, jsonarray, schema, attributemap):
-        parsed_array = json.loads(jsonarray)
-        results = self.arrayList()
-        caching_attribute_map = CachingAttributeMap(attributemap)
-
-        for record in parsed_array:
-            result = self.transform(self.process_context, record, schema, caching_attribute_map)
-            result_record = result.getRecord()
-            resultjson = None if result_record is None else json.dumps(result_record)
-            results.add(__RecordTransformResult__(result, resultjson))
-
-        return results
-
     def onTrigger(self, context: ProcessContext, session: ProcessSession):
-        original_flow_file = session.get()
-        if not original_flow_file:
+        flow_file = session.get()
+        if not flow_file:
             return
 
         record_reader = context.getProperty(self.RECORD_READER).asControllerService()
         record_writer = context.getProperty(self.RECORD_WRITER).asControllerService()
 
-        # flow_file = session.clone(original_flow_file)
+        try:
+            record_list = record_reader.read(flow_file, session)
+            if record_list is None:
+                self.logger.error("Reading flow file records returned None")
+                session.transfer(flow_file, self.REL_FAILURE)
+                return
+        except Exception:
+            self.logger.error("Failed to read flow file records due to the following error:\n{}".format(traceback.format_exc()))
+            session.transfer(flow_file, self.REL_FAILURE)
+            return
 
-        # flow_file_proxy = FlowFileProxy(session, flow_file)
-        # context_proxy = ProcessContextProxy(context, self)
-        # try:
-        #     result = self.transform(context_proxy, flow_file_proxy)
-        # except Exception:
-        #     self.logger.error("Failed to transform flow file due to error:\n{}".format(traceback.format_exc()))
-        #     session.remove(flow_file)
-        #     session.transfer(original_flow_file, self.REL_FAILURE)
-        #     return
+        flow_file_proxy = FlowFileProxy(session, flow_file)
+        context_proxy = ProcessContextProxy(context, self)
+        results = []
+        for record in record_list:
+            record_json = json.loads(record)
+            try:
+                result = self.transform(context_proxy, record_json, None, flow_file_proxy)
+                result_record = result.getRecord()
+                resultjson = None if result_record is None else json.dumps(result_record)
+                results.add(__RecordTransformResult__(result, resultjson))
+            except Exception:
+                self.logger.error("Failed to transform record due to the following error:\n{}".format(traceback.format_exc()))
+                session.transfer(flow_file, self.REL_FAILURE)
+                return
 
-        # if result.getRelationship() == "original":
-        #     session.remove(flow_file)
-        #     self.logger.error("Result relationship cannot be 'original', it is reserved for the original flow file, and transferred automatically in non-failure cases.")
-        #     session.transfer(original_flow_file, self.REL_FAILURE)
-        #     return
+        partitions = dict()
+        for result in results:
+            record_partition = result.getPartition()
+            if record_partition in partitions:
+                partitions[record_partition].add(result)
+            else:
+                partitions[record_partition] = [result]
 
-        # result_attributes = result.getAttributes()
-        # if result.getRelationship() == "failure":
-        #     session.remove(flow_file)
-        #     if result_attributes is not None:
-        #         for name, value in result_attributes.items():
-        #             original_flow_file.setAttribute(name, value)
-        #     if result.getContents() is not None:
-        #         self.logger.error("'failure' relationship should not have content, the original flow file will be transferred automatically in this case.")
-        #     session.transfer(original_flow_file, self.REL_FAILURE)
-        #     return
+        for _, partition_results in partitions.items():
+            partitioned_flow_file = session.create(flow_file)
+            record_writer.write([result.getRecordJson() for result in partition_results], partitioned_flow_file, session)
+            if result.getRelationship() == "success":
+                session.transfer(flow_file, self.REL_SUCCESS)
+            else:
+                session.transferToCustomRelationship(flow_file, result.getRelationship())
 
-        # if result_attributes is not None:
-        #     for name, value in result_attributes.items():
-        #         flow_file.setAttribute(name, value)
+            session.transfer(partitioned_flow_file, result.getRelationship())
 
-        # result_content = result.getContents()
-        # if result_content is not None:
-        #     session.write(flow_file, WriteCallback(result_content))
-
-        # if result.getRelationship() == "success":
-        #     session.transfer(flow_file, self.REL_SUCCESS)
-        # else:
-        #     session.transferToCustomRelationship(flow_file, result.getRelationship())
-        session.transfer(original_flow_file, self.REL_ORIGINAL)
+        session.transfer(flow_file, self.REL_ORIGINAL)
 
     @abstractmethod
     def transform(self, context: ProcessContextProxy, flowFile: FlowFileProxy) -> RecordTransformResult:
