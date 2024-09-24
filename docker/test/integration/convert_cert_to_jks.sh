6aa28bdca177c79cb0f73116bb297c14e03ddb98
Gabor Gyimesi <gamezbird@gmail.com>
2024-09-24 11:22:44 +0200

    fix shellcheck

--- docker/test/integration/convert_cert_to_jks.sh
+++ docker/test/integration/convert_cert_to_jks.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
+set -euo pipefail
 
-# Usage: ./create_jks.sh <directory> <ssl_key_path> <ssl_cert_path> <ca_cert_path>
+# Usage: ./create_jks.sh <base directory> <ssl_key_path> <ssl_cert_path> <ca_cert_path>
 
-# Input Arguments
 DIR=$1
 SSL_KEY_PATH=$2
 SSL_CERT_PATH=$3
 CA_CERT_PATH=$4
 
-# Keystore and Truststore Files
 KEYSTORE="$DIR/keystore.jks"
 TRUSTSTORE="$DIR/truststore.jks"
 PKCS12_FILE="$DIR/keystore.p12"
-
-# Common password
 PASSWORD="passw0rd1!"
 
-cat $CA_CERT_PATH >> $SSL_CERT_PATH
+cat "${CA_CERT_PATH}" >> "${SSL_CERT_PATH}"
 
-# Check if directory exists
 if [ ! -d "$DIR" ]; then
-    echo "Directory $DIR does not exist. Creating it."
     mkdir -p "$DIR"
 fi
 
-# Step 1: Convert SSL Key and Certificate to PKCS12 format
-echo "Converting SSL key and certificate to PKCS12 format..."
 openssl pkcs12 -export \
   -inkey "$SSL_KEY_PATH" \
   -in "$SSL_CERT_PATH" \
@@ -33,8 +26,6 @@ openssl pkcs12 -export \
   -out "$PKCS12_FILE" \
   -password pass:$PASSWORD
 
-# Step 2: Import the PKCS12 file into the JKS Keystore
-echo "Creating JKS keystore from PKCS12..."
 keytool -importkeystore \
   -destkeystore "$KEYSTORE" \
   -deststoretype jks \
@@ -46,14 +37,9 @@ keytool -importkeystore \
   -srcstorepass "$PASSWORD" \
   -noprompt
 
-# Step 3: Import the CA certificate into the Truststore
-echo "Creating JKS truststore and importing CA certificate..."
 keytool -importcert \
   -alias "nifi-cert" \
   -file "$CA_CERT_PATH" \
   -keystore "$TRUSTSTORE" \
   -storepass "$PASSWORD" \
   -noprompt
-
-echo "Keystore and Truststore creation complete."
-echo "Keystore location: $KEYSTORE"
