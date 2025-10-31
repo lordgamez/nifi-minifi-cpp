ec3c7e1ac6b3ae3f735fe7c411b2cc763c168630
Gabor Gyimesi <gamezbird@gmail.com>
2025-10-31 16:04:06 +0100

    upgrade_spdlog_fmt

--- cmake/Spdlog.cmake
+++ cmake/Spdlog.cmake
@@ -22,8 +22,8 @@ set(SPDLOG_FMT_EXTERNAL ON CACHE STRING "" FORCE)
 set(SPDLOG_SYSTEM_INCLUDES ON CACHE STRING "" FORCE)
 
 FetchContent_Declare(Spdlog
-        URL  https://github.com/gabime/spdlog/archive/refs/tags/v1.15.3.tar.gz
-        URL_HASH SHA256=15a04e69c222eb6c01094b5c7ff8a249b36bb22788d72519646fb85feb267e67
+        URL  https://github.com/gabime/spdlog/archive/refs/tags/v1.16.0.tar.gz
+        URL_HASH SHA256=8741753e488a78dd0d0024c980e1fb5b5c85888447e309d9cb9d949bdb52aa3e
         OVERRIDE_FIND_PACKAGE
         SYSTEM
         )
