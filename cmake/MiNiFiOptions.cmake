6338c9c89601f74ebf05c20e6629332ba9dddc99
Gabor Gyimesi <gamezbird@gmail.com>
2026-06-29 16:52:12 +0200

    add jemalloc

--- cmake/MiNiFiOptions.cmake
+++ cmake/MiNiFiOptions.cmake
@@ -156,6 +156,7 @@ add_minifi_multi_option(MINIFI_LIBARCHIVE_SOURCE "Retrieves libarchive from prov
 add_minifi_multi_option(MINIFI_LUA_SOURCE "Retrieves Lua from provided source" "BUILD;SYSTEM;CONAN" "BUILD")
 add_minifi_multi_option(MINIFI_SOL2_SOURCE "Retrieves Sol2 from provided source" "BUILD;SYSTEM;CONAN" "BUILD")
 add_minifi_multi_option(MINIFI_ARGPARSE_SOURCE "Retrieves Argparse from provided source" "BUILD;SYSTEM;CONAN" "BUILD")
+add_minifi_multi_option(MINIFI_JEMALLOC_SOURCE "Retrieves jemalloc from provided source" "BUILD;SYSTEM;CONAN" "BUILD")
 
 # Docker options
 
