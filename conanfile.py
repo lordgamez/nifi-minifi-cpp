729933fda5c422695ee6d8b03e0066945d0c7f61
Gabor Gyimesi <gamezbird@gmail.com>
2026-06-30 13:20:07 +0200

    add json schema validator

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ import shutil
 required_conan_version = ">=2.0"
 
 shared_requires = ("lz4/1.10.0", "openssl/3.6.2", "libcurl/8.20.0", "civetweb/1.16", "libxml2/2.15.3", "fmt/12.1.0", "spdlog/1.17.0", "catch2/3.14.0", "zlib/1.3.2", "zstd/1.5.7",
-                   "bzip2/1.0.8", "rocksdb/11.1.1@minifi/develop", "libarchive/3.8.7", "lua/5.4.6", "sol2/3.5.0", "argparse/3.2", "libsodium/1.0.22", "gsl-lite/1.1.0", "jsoncons/1.7.0")
+                   "bzip2/1.0.8", "rocksdb/11.1.1@minifi/develop", "libarchive/3.8.7", "lua/5.4.6", "sol2/3.5.0", "argparse/3.2", "libsodium/1.0.22", "gsl-lite/1.1.0", "jsoncons/1.7.0",
+                   "json-schema-validator/2.4.0")
 
 shared_sources = ("CMakeLists.txt", "libminifi/*", "extensions/*", "minifi_main/*", "behave_framework/*", "bin/*", "bootstrap/*", "cmake/*", "conf/*", "controller/*", "core-framework/*",
                   "docs/*", "encrypt-config/*", "etc/*", "examples/*", "extension-framework/*", "fips/*", "minifi-api/*", "packaging/*", "thirdparty/*", "docker/*", "LICENSE", "NOTICE",
@@ -69,6 +70,7 @@ class MiNiFiCppMain(ConanFile):
             tc.variables["MINIFI_LIBSSH2_SOURCE"] = "CONAN"
         if not self.options.skip_tests:
             tc.variables["MINIFI_BENCHMARK_SOURCE"] = "CONAN"
+            tc.variables["MINIFI_JSONSCHEMA_VALIDATOR_SOURCE"] = "CONAN"
 
         tc.generate()
 
